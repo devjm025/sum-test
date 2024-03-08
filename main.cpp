85c48709a19173c57ffae5fe43d7f5220e8a9b5e
devjm025 <wjm00627@swu.ac.kr>
2024-03-07 20:52:38 -0500

    Add main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+// main.cpp
+#include <stdio.h>
+// for using hearder, you have to include headers
+// and if you make main function you don't need to return 0
+#include "sum.h"
+
+int main() {
+	int s = sum(10);
+	printf("result=%d\n", s);
+}
+
